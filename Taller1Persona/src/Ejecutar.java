2995d52fa0c16c21012a5793e4bf56357b426085
Carolina Alvarez <carol02.carolina@gmail.com>
2022-05-19 20:59:44 -0500

    actualizac0iones

--- Taller1Persona/src/Ejecutar.java
+++ Taller1Persona/src/Ejecutar.java
@@ -1,9 +1,12 @@
+
 public class Ejecutar {
 public static void main (String args[]){
-    Cliente c = new Cliente("luis","jose",38, 29,1);
+    Cliente c = new Cliente("Jose", "Wolf",35, 1, 3918409);
+    System.out.println(c);
+    System.out.println(c.getcontadorPersonas());
     System.out.println(c.toString());
 
-    System.out.println(c.getcontadorPersonas());
+
 
     Empleado e1 =  new Empleado("ferguson","andres",40, 29L, 2500);
     System.out.println(e1.toString());
